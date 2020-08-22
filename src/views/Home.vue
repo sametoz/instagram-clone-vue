75da98ac59235ba52b07642d667cedbe167f9b1b
Samet Öz <samet.oz17@gmail.com>
2020-08-22 23:24:35 +0300

    -some css addition. -svg package implemented.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="home">
-    <CustomText>Selam</CustomText>
+    <CustomText class="a1">Selam</CustomText>
     <CustomText tag="p">Selam</CustomText>
-    <CustomText tag="b" size="xsmall">Selam</CustomText>
+    <CustomText tag="p" size="xsmall">Selam</CustomText>
+    <IconHome />
   </div>
 </template>
 
 <script>
 import CustomText from "@/components/CustomText";
+import IconHome from "../icons/home.svg";
 export default {
   name: "Home",
   components: {
-    CustomText
+    CustomText,
+    IconHome
   }
 };
 </script>
 
 <style>
-h1 {
-  color: red;
-  @media (--t) {
-    color: blue;
-  }
+.a1 {
+  color: rgb(var(--a72));
+}
+.a2 {
+  color: rgb(var(--b3f));
+}
+.a3 {
+  color: rgb(var(--ba8));
 }
 </style>
